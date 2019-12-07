8b487124448f0b2481f152e2a29ecbcfe67652d2
Ryan Nett <rnett@calpoly.edu>
2019-12-06 19:55:39 -0800

    add remove for id
    Signed-off-by: Ryan Nett <rnett@calpoly.edu>

--- src/commonMain/kotlin/com/rnett/delegates/Watchable.kt
+++ src/commonMain/kotlin/com/rnett/delegates/Watchable.kt
@@ -1,9 +1,13 @@
 package com.rnett.delegates
 
+import kotlin.random.Random
+
 
 interface Watchable<T> : ReadProvider<T> {
-    fun handleUpdate(handler: (T) -> Unit)
+    fun handleUpdate(handler: (T) -> Unit): Int
     fun refresh(newValue: T? = null)
+    fun removeHandler(handler: (T) -> Unit)
+    fun removeHandler(handlerId: Int)
 }
 
 interface MutableWatchable<T> : Watchable<T>, ReadWriteProvider<T>
@@ -16,19 +20,36 @@ inline fun <T> Watchable<T>.update(update: (T) -> Unit) {
 
 open class WatchableBase<T>(open val provider: ReadProvider<T>) : Watchable<T> {
 
-    private val handlers = mutableListOf<(T) -> Unit>()
+    private val handlers = mutableMapOf<Int, (T) -> Unit>()
 
-    override fun handleUpdate(handler: (T) -> Unit) {
-        handlers += handler
+    override fun handleUpdate(handler: (T) -> Unit): Int {
+        var id = Random.nextInt()
+
+        while (id in handlers) {
+            id = Random.nextInt()
+        }
+
+        handlers[id] = handler
+        return id
     }
 
     override fun refresh(newValue: T?) {
         val value = newValue ?: getValue()
-        handlers.forEach { it(value) }
+        handlers.values.forEach { it(value) }
     }
 
     override fun getValue(): T {
         return provider.getValue()
+    }
+
+    override fun removeHandler(handler: (T) -> Unit) {
+        handlers.entries.filter { it.value === handler || it.value == handler }.map { it.key }.forEach {
+            handlers.remove(it)
+        }
+    }
+
+    override fun removeHandler(handlerId: Int) {
+        handlers.remove(handlerId)
     }
 }
 
