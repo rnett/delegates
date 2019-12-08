8772d1cf73e76846ff5373469a8a861abcb334fb
Ryan Nett <rnett@calpoly.edu>
2019-12-07 18:33:56 -0800

    update of legacy delegates
    Signed-off-by: Ryan Nett <rnett@calpoly.edu>

--- src/commonMain/kotlin/com/rnett/delegates/legacy/Transform.kt
+++ src/commonMain/kotlin/com/rnett/delegates/legacy/Transform.kt
@@ -4,40 +4,19 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-data class ToReadOnlyDelegate<in R, T>(val base: ReadWriteProperty<R, T>) : ReadOnlyProperty<R, T> {
-    override fun getValue(thisRef: R, property: KProperty<*>): T {
-        return base.getValue(thisRef, property)
+inline fun <R, T, S> ReadOnlyProperty<R, T>.after(crossinline transform: (T) -> S) = object : ReadOnlyProperty<R, S> {
+    override inline fun getValue(thisRef: R, property: KProperty<*>): S = transform(this@after.getValue(thisRef, property))
+}
+
+inline fun <R, T, S> ReadWriteProperty<R, T>.after(crossinline getTransform: (T) -> S, crossinline setTransform: (S) -> T) = object : ReadWriteProperty<R, S> {
+    override inline fun getValue(thisRef: R, property: KProperty<*>): S = getTransform(this@after.getValue(thisRef, property))
+
+    override inline fun setValue(thisRef: R, property: KProperty<*>, value: S) {
+        this@after.setValue(thisRef, property, setTransform(value))
     }
 }
 
-data class LegacyReadOnlyTransform<in R, T, out S>(val base: ReadOnlyProperty<R, T>, val transform: (T) -> S) :
-    ReadOnlyProperty<R, S> {
-    override fun getValue(thisRef: R, property: KProperty<*>): S {
-        return transform(base.getValue(thisRef, property))
-    }
+fun <R, T> ReadWriteProperty<R, T>.asReadOnly() = object : ReadOnlyProperty<R, T> {
+    override inline fun getValue(thisRef: R, property: KProperty<*>): T = this@asReadOnly.getValue(thisRef, property)
 }
-
-@Deprecated(message = "Use ReadProvider if possible")
-fun <R, T, S> ReadOnlyProperty<R, T>.after(transform: (T) -> S) = LegacyReadOnlyTransform(this, transform)
-
-data class LegacyReadWriteTransform<in R, T, S>(
-    val base: ReadWriteProperty<R, T>,
-    val getTransform: (T) -> S,
-    val setTransform: (S) -> T
-) :
-    ReadWriteProperty<R, S> {
-    override fun getValue(thisRef: R, property: KProperty<*>): S {
-        return getTransform(base.getValue(thisRef, property))
-    }
-
-    override fun setValue(thisRef: R, property: KProperty<*>, value: S) {
-        base.setValue(thisRef, property, setTransform(value))
-    }
-}
-
-@Deprecated(message = "Use ReadWriteProvider if possible")
-fun <R, T, S> ReadWriteProperty<R, T>.after(getTransform: (T) -> S, setTransform: (S) -> T) =
-    LegacyReadWriteTransform(this, getTransform, setTransform)
-
-fun <R, T> ReadWriteProperty<R, T>.asReadOnly() = ToReadOnlyDelegate(this)
 
